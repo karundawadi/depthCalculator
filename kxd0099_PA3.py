48cb77e71bff7d27a2047f7d6e05b472d287d68e
Karun Dawadi <karundawadi@Karuns-MBP.lan>
2020-11-30 23:26:05 -0600

    - Added regex expression and completed replacement

--- kxd0099_PA3.py
+++ kxd0099_PA3.py
@@ -4,6 +4,7 @@
 # Date turend in : 11/30/2020
 
 import os as operting_system
+import re as regex_val
 
 # Reading the file
 test_file = open("input.txt",'r')
@@ -19,20 +20,27 @@ finally:
 depth = 0
 depth_carry = 0
 
-
 # Printing the contents of the file 
 for i in file_contents:
     if("{" in i or "}" in i):
         if(i.find("//")!=-1 or i.find("\"")!=-1):
+            # Using regex here
+            j = i
+            # Replaces all the values inside the "" with a whitespace
+            val_regex = "\".+\""
+            j = regex_val.sub(val_regex," ",j)
+
             # Means that sentence consists of \\ or " mark 
             # Using the relative position from the \\ to see if we need to count it or not
             # Since \\ would mean a complete line 
-            if(i.find("{")<i.find("//")): # Means that if { comes before an \\ sign 
+            if(i.find("{")<i.find("//") and j.find("{")!= -1): # Means that if { comes before an \\ sign 
+                # If this is inside " then we need to ignore it 
                 depth = depth +1
-            if(i.find("}")<i.find("//")): # Means that if } comes before an \\ sign 
-                depth = depth +1
+
+            if(i.find("}")<i.find("//") and j.find("}")!= -1): # Means that if } comes before an \\ sign 
+                # If this is inside " then we need to ignore it 
+                depth_carry = depth_carry - 1
             
-            print('This below line')
         else:    
             if(i.find("(")!= -1 and i.find("}") != -1):
                 # Meaning both exist in a single line 
