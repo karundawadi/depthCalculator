5c4ee8b47af7f19a8b12615ca8a6a0def289ae19
Karun Dawadi <karun.dawadi@mavs.uta.edu>
2020-12-02 12:29:19 -0600

    Shorter implementation

--- kxd0099_PA3.py
+++ kxd0099_PA3.py
@@ -23,37 +23,49 @@ depth_carry = 0
 
 # Printing the contents of the file 
 for i in file_contents:
-    if("{" in i or "}" in i):
-        if(i.find("//")!=-1 or i.find("\"")!=-1):
-            # Using regex here
-            j = i
-            # Replaces all the values inside the " " with a whitespace so there is no confusion because of this values
-            val_regex = "\".+\""
-            j = regex_val.sub(val_regex," ",j)
-
-            # Means that sentence consists of \\ or " mark 
-            # Using the relative position from the \\ to see if we need to count it or not
-            # Since \\ would mean a complete line 
-            if(i.find("{")<i.find("//") and j.find("{")!= -1): # Means that if { comes before an \\ sign 
-                # If this is inside " then we need to ignore it 
-                depth = depth +1
-
-            if(i.find("}")<i.find("//") and j.find("}")!= -1): # Means that if } comes before an \\ sign 
-                # If this is inside " then we need to ignore it 
-                depth_carry = depth_carry - 1
-            
-        else:    
-            if(i.find("(")!= -1 and i.find("}") != -1):
-                # Meaning both exist in a single line 
-                continue # Since both means that the depth is temporary and should not be counted 
-            
-            if(i.find("{")  != -1):
-                depth = depth + 1
-            
-            if(i.find("}") != -1):
-                depth_carry = depth_carry - 1     
+    j = i
+    # Replace value inside "quotation marks" with Regex 
+    val_regex = "\".+\""
+    j = regex_val.sub(val_regex," ",j) # Replaces the values inside the quotation marks with a whitespace
+    k = list(j)
+    for n in k:
+        if (n == '{'):
+            depth = depth+1
+        if (n == '}'):
+            depth_carry = depth_carry - 1 # Since we need to delay the count
 
     print(f"{depth} {i}")
     # Since we took } as depth_carry subtracting that and setting that to zero, for next rotation
     depth = depth + depth_carry 
     depth_carry = 0  
+
+
+# if("{" in i or "}" in i):
+#         if(i.find("//")!=-1 or i.find("\"")!=-1):
+#             # Using regex here
+#             j = i
+#             # Replaces all the values inside the " " with a whitespace so there is no confusion because of this values
+#             val_regex = "\".+\""
+#             j = regex_val.sub(val_regex," ",j)
+
+#             # Means that sentence consists of \\ or " mark 
+#             # Using the relative position from the \\ to see if we need to count it or not
+#             # Since \\ would mean a complete line 
+#             if(i.find("{")<i.find("//") and j.find("{")!= -1): # Means that if { comes before an \\ sign 
+#                 # If this is inside " then we need to ignore it 
+#                 depth = depth +1
+
+#             if(i.find("}")<i.find("//") and j.find("}")!= -1): # Means that if } comes before an \\ sign 
+#                 # If this is inside " then we need to ignore it 
+#                 depth_carry = depth_carry - 1
+            
+#         else:    
+#             if(i.find("(")!= -1 and i.find("}") != -1):
+#                 # Meaning both exist in a single line 
+#                 continue # Since both means that the depth is temporary and should not be counted 
+            
+#             if(i.find("{")  != -1):
+#                 depth = depth + 1
+            
+#             if(i.find("}") != -1):
+#                 depth_carry = depth_carry - 1     
